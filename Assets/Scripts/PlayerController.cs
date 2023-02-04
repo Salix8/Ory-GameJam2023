26842288d2d727d2864682d502d809dddd3acfba
SaulPachecoTrilles <saulpachecotrilles@gmail.com>
2023-02-04 12:33:35 +0100

    Ya estan añadidos los paquetes de sprites e inicio del script del playerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 20.0f;
+    public float jumpForce = 250.0f;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask whatIsGround;
+    public Transform groundCheck;
+
+    private Rigidbody2D rigidbody2d;
+    private Animator animator;
+    private bool facingRight = true;
+    private bool isGrounded = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            isGrounded = false;
+            animator.SetBool("IsJumping", true);
+            rigidbody2d.AddForce(new Vector2(0, jumpForce));
+        }
+
+        rigidbody2d.velocity = new Vector2(horizontal * speed, rigidbody2d.velocity.y);
+
+        if (horizontal > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (horizontal < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+        animator.SetBool("IsJumping", !isGrounded);
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
